909e64a5b25e196d0188677c82dafee222ee42d0
David Buckhurst <david@buckhurst.org>
2020-10-23 20:57:12 +0100

    Updates

---
+++ precache-manifest.30f19f5b793d38284255f601085b20e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88d58fc7c9760a73cb6f433fc521e8a9",
+    "url": "/dice-games/index.html"
+  },
+  {
+    "revision": "81c024fb922328bef067",
+    "url": "/dice-games/static/css/main.30bf7cad.chunk.css"
+  },
+  {
+    "revision": "be3407d1820afd9f9a8b",
+    "url": "/dice-games/static/js/2.21fa76d3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/dice-games/static/js/2.21fa76d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81c024fb922328bef067",
+    "url": "/dice-games/static/js/main.dc85c66f.chunk.js"
+  },
+  {
+    "revision": "c2ffb272d3eeadd93e97",
+    "url": "/dice-games/static/js/runtime-main.94b1f3ab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dice-games/static/media/logo.5d5d9eef.svg"
+  }
+]);
